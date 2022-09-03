b83bf65f5f1fa91500ebf3ee033e552faecd6ff4
Candyce10 <candycematthews@yahoo.com>
2022-09-02 19:16:09 -0700

    template created

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('about/', views.About.as_view(), name="about")
+    path('about/', views.About.as_view(), name="about"),
+    path('contact/', views.Contact.as_view(), name="contact"),
+    path('services/', views.Services.as_view(), name="services"),
+    path('workshops/', views.Workshops.as_view(), name="workshops"),
+    path('testimonials/', views.Testimonials.as_view(), name="testimonials"),
+    
 ]
--- rg_consulting_project/settings.py
+++ rg_consulting_project/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for rg_consulting_project project.
 
@@ -11,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,9 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# 
+# 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
