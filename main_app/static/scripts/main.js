6f6751d81efba0f98af9c823ababe362aeaca2d3
Candyce10 <candycematthews@yahoo.com>
2022-10-17 22:12:24 -0700

    more spanish conversion implemented

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -3,17 +3,28 @@ let data ={
     "english":
     {
         "contact":  "Contact",
-        "footer": " Over 35 years helping people & businesses with financial crisis management"
+        "footer": "Over 35 years helping people & businesses with financial crisis management",
+        "construction": "Under Construction",
+        "construction2": "We're working to finish the development of this site. Please check back soon!",
+        "headqt": '"Helping minorites and businesses reach their funding goals"'
     },
     "spanish":
     {
         "contact": "Contacto",
-        "footer": "más de 35 años"
+        "footer": "más de 35 años",
+        "construction": "En Construcción",
+        "construction2": "Esta página web está en construcción. Por favor vuelva más tarde.",
+        "headqt": '"Ayudar a las minorías y a las empresas con sus objetivos de financiación"',
+
+
     }
 }
 const langEl = document.querySelector('.langWrap');
 const contactEl = document.querySelector('.contact');
 const footerEl = document.querySelector('.footer');
+const constructionEl = document.querySelector('.construction');
+const constructionEl2 = document.querySelector('.construction2');
+const headqtEl = document.querySelector('.headqt');
 const link = document.querySelectorAll('a');
 
 link.forEach(el =>{
@@ -25,6 +36,9 @@ link.forEach(el =>{
 
         contactEl.textContent = data[attr].contact
         footerEl.textContent = data[attr].footer
+        constructionEl.textContent = data[attr].construction
+        constructionEl2.textContent = data[attr].construction2
+        headqtEl.textContent = data[attr].headqt
     })
 })
 console.log("hi")
